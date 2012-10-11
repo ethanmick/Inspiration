57cb7a534306428576dcb106a55b93e3090c0fc8
Ethan Mick <ethanmski@gmail.com>
2012-10-11 16:39:15 -0400

    Added Stream Text.

---
+++ Inspiration/StreamText.m
@@ -0,0 +1,13 @@
+//
+//  StreamText.m
+//  Inspiration
+//
+//  Created by Ethan Mick on 10/11/12.
+//  Copyright (c) 2012 Ethan Mick. All rights reserved.
+//
+
+#import "StreamText.h"
+
+@implementation StreamText
+
+@end
