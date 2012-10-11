b309a00b35a3a9697db86928a449e1190a267503
Ethan Mick <ethanmski@gmail.com>
2012-10-11 15:06:18 -0400

    Added the Stream Layout file.

---
+++ Inspiration/StreamLayout.m
@@ -0,0 +1,13 @@
+//
+//  StreamLayout.m
+//  Inspiration
+//
+//  Created by Ethan Mick on 10/11/12.
+//  Copyright (c) 2012 Ethan Mick. All rights reserved.
+//
+
+#import "StreamLayout.h"
+
+@implementation StreamLayout
+
+@end
