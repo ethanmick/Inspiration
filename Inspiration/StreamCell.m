dcb4626bbae6c51b039d8fdca0bdf0dc69b9398c
Ethan Mick <ethanmski@gmail.com>
2012-10-11 15:00:04 -0400

    Adding Cell subclasses to control their content view.

---
+++ Inspiration/StreamCell.m
@@ -0,0 +1,31 @@
+//
+//  StreamCell.m
+//  Inspiration
+//
+//  Created by Ethan Mick on 10/11/12.
+//  Copyright (c) 2012 Ethan Mick. All rights reserved.
+//
+
+#import "StreamCell.h"
+
+@implementation StreamCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
