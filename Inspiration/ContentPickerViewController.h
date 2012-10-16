473dd5796bbdecdbedde1dcfa6e36d894c7407df
Ethan Mick <ethanmski@gmail.com>
2012-10-16 14:45:28 -0400

    Adding in a picker to add content.

---
+++ Inspiration/ContentPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContentPickerViewController.h
+//  Inspiration
+//
+//  Created by Ethan Mick on 10/16/12.
+//  Copyright (c) 2012 Ethan Mick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentPickerViewController : UITableViewController
+
+@end
