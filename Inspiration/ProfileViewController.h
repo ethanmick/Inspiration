a7a37b2fd3835cb341afb2f8d3cbb89d27d94e8a
Ethan Mick <ethanmski@gmail.com>
2012-10-16 10:56:37 -0400

    Adding ProfileViewController.

---
+++ Inspiration/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Inspiration
+//
+//  Created by Ethan Mick on 10/15/12.
+//  Copyright (c) 2012 Ethan Mick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
