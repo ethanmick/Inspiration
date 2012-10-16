c231113030488ee229de56245d0dd25af021da08
Ethan Mick <ethanmski@gmail.com>
2012-10-16 15:08:15 -0400

    Add controller for controlling text.

---
+++ Inspiration/AddTextViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddTextViewController.h
+//  Inspiration
+//
+//  Created by Ethan Mick on 10/16/12.
+//  Copyright (c) 2012 Ethan Mick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTextViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@end
