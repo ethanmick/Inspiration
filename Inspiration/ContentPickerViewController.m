a2c070e9402a3bcc2d913b3f4990c79e66d469c9
Ethan Mick <ethanmski@gmail.com>
2012-10-17 11:51:33 -0400

    Removed unnecessary code.

--- Inspiration/ContentPickerViewController.m
+++ Inspiration/ContentPickerViewController.m
@@ -14,51 +14,13 @@
 
 @implementation ContentPickerViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    
-    ///
-    /// Change The navigation back button to make sure the user understands
-    /// we are saving the info.
-    ///
-    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Save"
-                                                                             style:UIBarButtonItemStylePlain
-                                                                            target:nil
-                                                                            action:nil];
-
-}
-
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
     [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    ///
-    /// Get an Image
-    ///
-    if (indexPath.row == 1) {
-        
-    }
+
 }
 
 @end
