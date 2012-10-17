83365092b53371ee0fabb6ccc82246a0b8ef5219
Ethan Mick <ethanmski@gmail.com>
2012-10-17 16:35:04 -0400

    Added in a method for selecting items.

---
+++ Inspiration/SelectedItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectedItemViewController.h
+//  Inspiration
+//
+//  Created by Ethan Mick on 10/17/12.
+//  Copyright (c) 2012 Ethan Mick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectedItemViewController : UIViewController
+
+@end
---
+++ Inspiration/SelectedItemViewController.m
@@ -0,0 +1,38 @@
+//
+//  SelectedItemViewController.m
+//  Inspiration
+//
+//  Created by Ethan Mick on 10/17/12.
+//  Copyright (c) 2012 Ethan Mick. All rights reserved.
+//
+
+#import "SelectedItemViewController.h"
+
+@interface SelectedItemViewController ()
+
+@end
+
+@implementation SelectedItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
